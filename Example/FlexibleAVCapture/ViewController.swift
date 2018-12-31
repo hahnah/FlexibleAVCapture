90975bdb963ae2f3353c4f66c294062068964cc1
hahnah <superhahnah@gmail.com>
2019-01-01 02:08:27 +0900

    Implement example

--- Example/FlexibleAVCapture/ViewController.swift
+++ Example/FlexibleAVCapture/ViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 import FlexibleAVCapture
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FlexibleAVCaptureViewControllerDelegate {
+    
+    let vc: FlexibleAVCaptureViewController = FlexibleAVCaptureViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let vc: FlexibleAVCaptureViewController = FlexibleAVCaptureViewController()
-        self.present(vc, animated: true, completion: nil)
+        self.showFlexibleAVCaptureView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func showFlexibleAVCaptureView() {
+        vc.flexibleCaptureDelegate = self
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func didCapture(withFileURL fileURL: URL) {
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: fileURL)
+        }) { saved, error in
+            DispatchQueue.main.async {
+                let success = saved && (error == nil)
+                let title = success ? "Success" : "Error"
+                let message = success ? "Movie saved." : "Failed to save movie."
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+                self.vc.present(alert, animated: true, completion: nil)
+            }
+        }
     }
 
 }
